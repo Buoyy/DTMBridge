959b072f98c5ecbf7d1064d7f21593e933055be7
Buoyy <81320656+Buoyy@users.noreply.github.com>
2024-12-29 09:03:40 +0530

    Comment DiscordListener

--- src/main/java/com/github/buoyy/dtm/DiscordListener.java
+++ src/main/java/com/github/buoyy/dtm/DiscordListener.java
@@ -6,13 +6,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
 
+//ListenerAdapter for listening to message event
 public class DiscordListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         String chatChannelId = "Your channel id here";
-        if (event.getAuthor().isBot() || event.getAuthor().isSystem()) return;
-        if (!event.getChannel().getId().equals(chatChannelId)) return;
-        String msg = String.format("<<%s>> %s", (ChatColor.RED + event.getAuthor().getEffectiveName()), (ChatColor.GRAY + event.getMessage().getContentDisplay()));
-        Bukkit.broadcastMessage(msg);
+        if (event.getAuthor().isBot() || event.getAuthor().isSystem()) return; //Return if message is from bot or system; could cause infinite loop if not used
+        if (!event.getChannel().getId().equals(chatChannelId)) return; // Channel check
+        String msg = String.format("<<%s>> %s", (ChatColor.RED + event.getAuthor().getEffectiveName()), (ChatColor.GRAY + event.getMessage().getContentDisplay())); //Will change colouring later
+        Bukkit.broadcastMessage(msg); 
     }
 }
