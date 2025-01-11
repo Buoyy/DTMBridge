0739dcd11dc9eb2f25ace9e57eb2699c13b08f06
Buoyy <saarthak0902@gmail.com>
2025-01-11 18:11:29 +0530

    Another intermediary

--- src/main/java/com/github/buoyy/dtm/utils/LinksManager.java
+++ src/main/java/com/github/buoyy/dtm/utils/LinksManager.java
@@ -2,6 +2,7 @@ package com.github.buoyy.dtm.utils;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.HashMap;
 import com.github.buoyy.dtm.utils.Account;
 import com.github.buoyy.dtm.utils.TempAccount;
 import org.bukkit.entity.Player;
@@ -10,10 +11,12 @@ import net.dv8tion.jda.api.entities.Member;
 //TODO: Keep genKey() method here instead
 public class LinksManager {
     private ArrayList<TempAccount> temps;
+    private HashMap<Player, String> idConnector;
     private ArrayList<Account> accounts;
     public LinksManager() {
         this.temps = new ArrayList<>();
         this.accounts = new ArrayList<>();
+        this.idConnector = new HashMap<>();
     }
     public TempAccount createTemp(Player player) {
         TempAccount temp = new TempAccount(player);
@@ -22,6 +25,9 @@ public class LinksManager {
     }
     public ArrayList<TempAccount> getTemps() {
         return temps;
+    }
+    public HashMap<Player, String> getConnector() {
+        return idConnector;
     }
     public boolean verifyAndLink(Player player, Member user, String key) {
         Iterator<TempAccount> it = temps.iterator();
