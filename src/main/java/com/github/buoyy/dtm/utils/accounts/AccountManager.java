a6dc01bb0dae95ba568e27b74e3ff4570418da4d
Buoyy <81320656+Buoyy@users.noreply.github.com>
2025-01-14 20:03:39 +0530

    update sr

--- src/main/java/com/github/buoyy/dtm/utils/accounts/AccountManager.java
+++ src/main/java/com/github/buoyy/dtm/utils/accounts/AccountManager.java
@@ -7,38 +7,50 @@ import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public class AccountManager {
     private final ArrayList<Account> accounts;
-    private final YAMLLoader loader;
     private final JDA jda;
     private final FileConfiguration config;
+    private final Set<String> yamlIndices;
+    
     public AccountManager(JDA jda, YAMLLoader loader) {
         this.accounts = new ArrayList<>();
-        this.loader = loader;
         this.config = loader.getConfig();
         this.jda = jda;
-        loadAccounts(loader);
+        this.yamlIndices = this.config.getKeys(false);
+        loadSavedAccounts();
     }
-    public void loadAccounts(YAMLLoader loader) {
-        Set<String> yamlIndices = config.getKeys(false);
+    public void loadSavedAccounts() {
         for (String i : yamlIndices) {
             String mcId = config.getString(i+".mc-id");
             String discordId = config.getString(i+"discord-id");
+            String key = config.getString(i+".key");
             Account account = new Account();
             account.setPlayer(Bukkit.getPlayer(mcId));
             account.setUser(jda.getUserById(discordId));
+            account.setKey(key);
             accounts.add(account);
         }
     }
-    public void addAccount(String mcId, String discordId) {
+    public boolean hasAccount(String mcId) {
+      for (String i : yamlIndices) {
+        if (mcId.equals(config.getString(i+".mc-id"))) return true;
+      }
+      return false;
+    }
+    public String getLinkedUser(String mcId) {
+      for (String i : yamlIndices) {
+        if (hasAccount(mcId)) {
+          return jda.getUserById(config.getString(i+".discord-id"));
+        }
+      }
+    }
+    public void addAccount(String mcId) {
       int next = yamlIndices.size();
       config.set(next+".mc-id", mcId);
-      config.set(next+".discord-id", discordId);
-      loader.saveConfig();
-    }
-    public YAMLLoader getLoader() {
-        return loader;
+      config.set(next+".discord-id", null);
     }
     public ArrayList<Account> getAccounts() {
         return accounts;
