b8e52618832591c2ee1909e37c9f1078fbcc450c
Buoyy <saarthak0902@gmail.com>
2026-05-28 10:39:11 +0530

    Remove older unused files

--- src/main/java/com/github/buoyy/dtm/commands/mc/MCSubCommand.java
+++
@@ -1,7 +0,0 @@
-package com.github.buoyy.dtm.commands.mc;
-
-import org.bukkit.command.CommandSender;
-
-public interface MCSubCommand {
-    boolean execute(CommandSender sender, String[] args);
-}
--- src/main/java/com/github/buoyy/dtm/listeners/discord/DiscordChatListener.java
+++ src/main/java/com/github/buoyy/dtm/listeners/discord/DiscordChatListener.java
@@ -1,41 +1,48 @@
 package com.github.buoyy.dtm.listeners.discord;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+import com.github.buoyy.dtm.MainManager;
+
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.entities.Guild;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.jetbrains.annotations.NotNull;
 
-// Send messages in the Minecraft server chat
-public class DiscordChatListener extends ListenerAdapter {
-
-    private final TextChannel channel;
-    private final Guild guild;
+// Broadcasts the messages sent in Discord to Minecraft
+public class DiscordChatListener extends ListenerAdapter
+{
+    private final TextChannel chatChannel;
     private final String dcChatMsg;
-
-    public DiscordChatListener(Guild guild, TextChannel channel, FileConfiguration config) {
-        this.guild = guild;
-        this.channel = channel;
-        this.dcChatMsg = config.getString("dc-chat-msg");
+    private final Sound sound;
+    private final float soundVolume, soundPitch;
+    
+    public DiscordChatListener(MainManager manager)
+    {
+        this.chatChannel = manager.getChatChannel();
+        this.dcChatMsg = manager.getPlugin().getConfig().getString("dc-chat-msg");
+        this.sound = Sound.valueOf(manager.getPlugin().getConfig().getString("notif.sound"));
+        this.soundVolume = (float)manager.getPlugin().getConfig().getDouble("notif.volume");
+        this.soundPitch = (float)manager.getPlugin().getConfig().getDouble("notif.pitch");
     }
 
     @Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+    public void onMessageReceived(MessageReceivedEvent event) 
+    {
         if (event.getAuthor().isBot() || event.getAuthor().isSystem())
-            return; //Return if message is from bot or system; could cause infinite loop if not used
-        if (nullCheck()) {
-            if (!event.getChannel().equals(channel)) return; // Channel check
-            String msg = dcChatMsg.replace("{player}", event.getAuthor().getEffectiveName())
-                    .replace("{msg}", event.getMessage().getContentDisplay());
-            String formattedMsg = ChatColor.translateAlternateColorCodes('&', msg);
-            Bukkit.broadcastMessage(formattedMsg);
-        }
-    }
+            return;
+        if (!chatChannel.equals((TextChannel)event.getChannel()))
+            return;
+        String msg = dcChatMsg
+                        .replace("{player}", event.getAuthor().getEffectiveName())
+                        .replace("{msg}", event.getMessage().getContentStripped());
+        String formattedMsg = ChatColor.translateAlternateColorCodes('&', msg);
+        Bukkit.broadcastMessage(formattedMsg);
 
-    private boolean nullCheck() {
-        return !(guild == null || channel == null);
+        // Notification sound
+        for (Player player : Bukkit.getOnlinePlayers().stream().toList())
+            player.playSound(player.getLocation(), sound, soundVolume, soundPitch);
     }
 }
